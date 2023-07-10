2931303500f4d3921d632881ce4ae0f56b0a8b5e
seohee0925 <appleid21@sookmyung.ac.kr>
2023-07-10 17:56:26 +0900

    Feat: Template 구현

--- MatchZzic/main/models.py
+++ MatchZzic/main/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Post (Model.model):
+    place = models.TextField('동행할 여행지')
--- MatchZzic/main/urls.py
+++ MatchZzic/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from main.views import main
 
 urlpatterns = [
-    
+    path('main/', main, name='main'),
 ]
