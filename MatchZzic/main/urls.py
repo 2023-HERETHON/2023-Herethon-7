385a389677e6e17dc68be1a35518c0e2accabc0d
seohee0925 <appleid21@sookmyung.ac.kr>
2023-07-12 07:38:53 +0900

    Feat: 장소 입력받기

--- MatchZzic/main/urls.py
+++ MatchZzic/main/urls.py
@@ -3,5 +3,8 @@ from . import views
 from main.views import main
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('main/', main, name='main'),
+    path('createform/', views.createform, name='createform'),
+    # 데이터 처리할 url 등록
 ]
--- MatchZzic/main/views.py
+++ MatchZzic/main/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Post
 
 # Create your views here.
 def main(request):
     return render(request, 'main.html')
+
+def createform(request):
+    if request.method == 'POST':
+        place = Post()
+        place.place = request.POST['place']
+        place.save()
+        
+        return redirect('main')
